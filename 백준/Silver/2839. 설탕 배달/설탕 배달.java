f3d492b3b207ed74ae2a6939f1310bf61eea96da
HyeJu Han <hy2jv@naver.com>
2024-02-07 20:26:12 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while (n>0) {
+            if (n%5==0) {
+                cnt += n/5;
+                break;
+            }
+            n -= 3; cnt++;
+        }
+        if(n<0) System.out.println("-1");
+        else System.out.println(cnt);
+    }
+}
