d734ef170c3d31f3af3f0f31bdbc323ff719b0d7
HyeJu Han <metaannotation@gmail.com>
2024-02-26 15:49:29 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,25 +1,9 @@
 n = int(input())
 money = 1000 - n
 result = 0
-while money > 0:
-    if money >= 500:
-        result += (money // 500)
-        money -= 500 * (money // 500)
-    elif money >= 100:
-        result += (money // 100)
-        money -= 100 * (money // 100)
-    elif money >= 50:
-        result += (money // 50)
-        money -= 50 * (money // 50)
-    elif money >= 10:
-        result += (money // 10)
-        money -= 10 * (money // 10)
-    elif money >= 5:
-        result += (money // 5)
-        money -= 5 * (money // 5)
-    else:
-        result += (money // 1)
-        money -= 1 * (money // 1)
+arr = [500, 100, 50, 10, 5, 1]
 
-result += (money // 1)
+for i in arr:
+    result += money // i
+    money %= i
 print(result)
